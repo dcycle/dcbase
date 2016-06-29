52aa83434c17610e64ea3deb78c3177710562270
alberto56 <albert@mediatribe.net>
2016-06-29 16:46:31 -0400

    Fix automated tests

--- tests/dcbase-test.sh
+++ tests/dcbase-test.sh
@@ -1,12 +1,13 @@
-PWD=$(pwd)
-cd "$PWD"/examples/example-001 && mkdir core
-cp -r "$PWD"/modules "$PWD"/examples/example-001/core/modules
-cp -r "$PWD"/tests "$PWD"/examples/example-001/core/tests
-cp -r "$PWD"/README.md "$PWD"/examples/example-001/core/README.md
-cd "$PWD"/examples/example-001 && ./example-script.sh
-cd "$PWD"/examples/example-001 && ./example-script.sh usage
-cd "$PWD"/examples/example-001 && ./example-script.sh help say-hello-world
-cd "$PWD"/examples/example-001 && ./example-script.sh say-hello-world
-cd "$PWD"/examples/example-001 && ./example-script.sh say-hello-world --type=caps
-cd "$PWD"/examples/example-001 && ./example-script.sh say-hello-world --type=caps --simulation-mode=true
-cd "$PWD"/examples/example-001 && ./example-script.sh self-test
+BASE=$(pwd)
+cd "$BASE"/examples/example-001 && mkdir core
+cp -r "$BASE"/modules "$BASE"/examples/example-001/core/modules
+cp -r "$BASE"/tests "$BASE"/examples/example-001/core/tests
+cp -r "$BASE"/README.md "$BASE"/examples/example-001/core/README.md
+cd "$BASE"/examples/example-001 && ./example-script.sh
+cd "$BASE"/examples/example-001 && ./example-script.sh usage
+cd "$BASE"/examples/example-001 && ./example-script.sh help say-hello-world
+cd "$BASE"/examples/example-001 && ./example-script.sh say-hello-world
+cd "$BASE"/examples/example-001 && ./example-script.sh say-hello-world --type=caps
+cd "$BASE"/examples/example-001 && ./example-script.sh say-hello-world --type=caps --simulation-mode=true
+cd "$BASE"/examples/example-001 && ./example-script.sh self-test
+rm -rf "$BASE"/examples/example-001/core
