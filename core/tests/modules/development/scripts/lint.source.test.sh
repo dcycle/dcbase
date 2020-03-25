4889d8a379982ec88fff169c10a9f6582e97847e
Albert Albala <albert@mediatribe.net>
2020-03-25 08:53:06 -0400

    Use this as a starterkit rather than a git submodule, move from Travis to CircleCI for testing (#4)

--- core/tests/modules/development/scripts/lint.source.test.sh
+++ core/tests/modules/development/scripts/lint.source.test.sh
@@ -4,13 +4,14 @@ BASE=$(pwd)
 DOCKER='echo docker'
 GlobalSCRIPTDIR="$BASE"
 
-NEEDLE="$BASE"/Dockerfile-linter
-MESSAGE="Dockerfile-linter exists."
-source ./core/modules/testing/scripts/assert-file.source.sh
-
 HAYSTACK=$(source "$BASE"/core/modules/development/scripts/lint.source.sh)
-NEEDLE="docker build -f Dockerfile-linter"
-MESSAGE="Linter builds a docker container"
+
+echo "AAA"
+echo "$HAYSTACK"
+echo "ZZZ"
+
+NEEDLE="./core/tests/modules/temporary/hooks/cleanup/cleanup.source.test.sh"
+MESSAGE="Linting source finds ALL files including the randomly-selected $NEEDLE"
 source ./core/modules/testing/scripts/assert.source.sh
 
 source ./core/modules/testing/scripts/cleanup.source.sh
